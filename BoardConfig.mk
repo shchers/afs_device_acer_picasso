6e571fb2ea41f4c0ede50f07b33e21db56f8ec8a
Sergey Shcherbakov <shchers@gmail.com>
2012-04-10 00:01:45 +0300

    [wifi] Updated pathes to Broadcom WiFi module firmwares
    Signed-off-by: Sergey Shcherbakov <shchers@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,11 +54,11 @@ WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
 BOARD_WLAN_DEVICE                := bcm4329
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
-WIFI_DRIVER_MODULE_PATH          := "/system/modules/bcm4329.ko"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
 WIFI_DRIVER_MODULE_NAME          :=  "bcm4329"
-WIFI_DRIVER_MODULE_ARG           :=  "iface_name=wlan0 firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/system/etc/wifi/bcmdhd.cal"
+WIFI_DRIVER_MODULE_ARG           :=  "iface_name=wlan0 firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/system/etc/wifi/bcmdhd.cal"
 
 BOARD_USES_GENERIC_AUDIO := false
 

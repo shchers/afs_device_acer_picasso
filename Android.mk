3b933d626d904dd8d61f4a041389136c85e6a6ce
Sergey Shcherbakov <shchers@gmail.com>
2012-04-09 23:43:57 +0300

    Update Android.mk to build modules in sub-folders
    Solved issue with building audio.primary.picasso.so
    
    Signed-off-by: Sergey Shcherbakov <shchers@gmail.com>

--- Android.mk
+++ Android.mk
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(my-dir)
+LOCAL_PATH := $(call my-dir)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+

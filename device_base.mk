dc8b90519208622feaaeb930fa8c7253773c29df
Sergey Shcherbakov <shchers@gmail.com>
2012-07-14 23:44:36 +0300

    [cfg] Added list of supported media codecs
    According to Android JB architecture supported codecs and
    particular OMX components should be defined in the
    media_codecs.xml. This file will be uploaded to the mediaserver
    service during the initialization and will be used for encoding
    or/and decoding.
    
    Signed-off-by: Sergey Shcherbakov <shchers@gmail.com>

--- device_base.mk
+++ device_base.mk
@@ -88,7 +88,8 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # media config xml file
 PRODUCT_COPY_FILES += \
-    device/acer/picasso/media_profiles.xml:system/etc/media_profiles.xml
+    device/acer/picasso/media_profiles.xml:system/etc/media_profiles.xml \
+    device/acer/picasso/media_codecs.xml:system/etc/media_codecs.xml
 
 # Bluetooth config file
 PRODUCT_COPY_FILES += \
